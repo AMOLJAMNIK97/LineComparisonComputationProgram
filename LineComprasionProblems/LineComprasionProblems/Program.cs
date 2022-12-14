494a1d2b055688a7c5ad6cc676a70534365cacc4
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-14 23:17:19 +0530

    [amol]- Compare method

--- LineComprasionProblems/LineComprasionProblems/Program.cs
+++ LineComprasionProblems/LineComprasionProblems/Program.cs
@@ -12,35 +12,27 @@ class Program
     public static void main(String[] args)
     {
 
-        // Get some Strings to compare
-        String s1 = "A";
-        String s2 = "A";
-        String s3 = "A";
-        String s4 = new String("A");
+        String str1 = "Grand Theft Auto";
+        String str2 = "Assassin Creed";
+        String str3 = "Call of Duty";
+        String str4 = "Need for Speed";
+        String str5 = "Grand Theft Auto";
 
-        // Compare s1 and s2
-        // It should return true as they both
-        // refer to same object in memory
-        Console.WriteLine(s1 + " == " + s2
-                         + ": " + (s1 == s2));
+        Console.WriteLine(str1.CompareTo(str2));
+        // Since 'A' is greater than 'G' by 6 characters, so it will return 6
 
-        // Compare s1 and s3
-        // It should return true as they both
-        // refer to same object in memory
-        Console.WriteLine(s1 + " == " + s3
-                         + ": " + (s1 == s3));
+        Console.WriteLine(str2.CompareTo(str3));
+        // Since 'C' is smaller than 'A' by 2 characters, so it will return -2
 
-        // Compare s2 and s3
-        // It should return true as they both
-        // refer to same object in memory
-        Console.WriteLine(s2 + " == " + s3
-                         + ": " + (s2 == s3));
+        Console.WriteLine(str3.CompareTo(str4));
+        //Since 'N' is smaller than 'C' by 11 characters, so it will return -11
 
-        // Compare s1 and s4
-        // It should return false as they both
-        // refer to different object in memory
-        Console.WriteLine(s1 + " == " + s4
-                         + ": " + (s1 == s4));
+        Console.WriteLine(str4.CompareTo(str1));
+        //Since 'G' is Greater than 'N' by 7 characters, so it will return 7
+
+        Console.WriteLine(str1.CompareTo(str5));
+        //Strings are equal
+
     }
 
 }
